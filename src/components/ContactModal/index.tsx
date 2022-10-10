0a473f7925fddc31c65662ca55414ba5c66aaab7
gustavohenri316 <gustavohenri316@gmail.com>
2022-10-10 17:09:38 -0300

    atualizando cvs

--- src/components/ContactModal/index.tsx
+++ src/components/ContactModal/index.tsx
@@ -9,40 +9,46 @@ import {
   Title,
   Close,
   Tel,
-  TelContent
+  TelContent,
 } from "./styles";
-import Modal from 'react-modal'
+import Modal from "react-modal";
 import { X } from "phosphor-react";
 import { useState } from "react";
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
-import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
 import React from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
-  onRequestClose: () => void
+  onRequestClose: () => void;
 }
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   props,
-  ref,
+  ref
 ) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export function ContactModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [ddd, setDdd] = useState('')
-  const [textarea, setTextArea] = useState('')
+export function ContactModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [ddd, setDdd] = useState("");
+  const [textarea, setTextArea] = useState("");
   const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
     setOpen(true);
   };
 
-  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -95,7 +101,7 @@ export function ContactModal({ isOpen, onRequestClose }: NewTransactionModalProp
                 />
               </TelContent>
               <TelContent>
-                <Label htmlFor="phone">Telefone</Label>
+                <Label htmlFor="phone">Telephone</Label>
                 <Input
                   type="text"
                   id="phone"
@@ -104,7 +110,6 @@ export function ContactModal({ isOpen, onRequestClose }: NewTransactionModalProp
                   onChange={(e) => setPhone(e.target.value)}
                 />
               </TelContent>
-
             </Tel>
             <Label htmlFor="message">Message </Label>
             <TextArea
@@ -114,9 +119,19 @@ export function ContactModal({ isOpen, onRequestClose }: NewTransactionModalProp
               onChange={(e) => setTextArea(e.target.value)}
             />
             <input type="hidden" name="_captcha" value="false"></input>
-            <input type="hidden" name="_next" value="https://gustavoh-jpg.vercel.app/"></input>
+            <input
+              type="hidden"
+              name="_next"
+              value="https://gustavoh-jpg.vercel.app/"
+            ></input>
             <Button
-              disabled={name === '' || email === '' || textarea === "" || phone === '' || ddd === ''}
+              disabled={
+                name === "" ||
+                email === "" ||
+                textarea === "" ||
+                phone === "" ||
+                ddd === ""
+              }
               type="submit"
               onClick={handleClick}
             >
@@ -124,13 +139,16 @@ export function ContactModal({ isOpen, onRequestClose }: NewTransactionModalProp
             </Button>
           </Form>
           <Snackbar open={open} autoHideDuration={10000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            <Alert
+              onClose={handleClose}
+              severity="success"
+              sx={{ width: "100%" }}
+            >
               E-mail enviado com sucesso!!
             </Alert>
           </Snackbar>
         </Content>
       </Modal>
-
     </Container>
-  )
+  );
 }
